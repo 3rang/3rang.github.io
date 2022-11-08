a3b7933837a47b2c61975283d08d36662d469f2c
Tarang <tarang@debian-BULLSEYE-live-builder-AMD64>
2022-11-08 13:38:30 +0100

    basic info page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import profileImage from './img/dp.JPG'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+	<h1>Tarang Patel </h1>
+	<h1> Embedded Software Engineer </h1>
+        <img src={profileImage} alt="profile-image" />
+
+    </div>
+  );
+}
+
+export default App;
